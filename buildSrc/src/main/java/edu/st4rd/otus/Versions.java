efe0efe16da8ebbd27e327d3496be7697c15fcef
alexey-polyakov <alexey.polyakov.p@gmail.com>
2022-01-22 14:03:06 +0300

    [main] - created initial structure of project

---
+++ buildSrc/src/main/java/edu/st4rd/otus/Versions.java
@@ -0,0 +1,11 @@
+package edu.st4rd.otus;
+
+public final class Versions {
+
+    private Versions() {
+    }
+
+    public static final String SPRING_BOOT_DEPENDENCIES = "2.6.0";
+    public static final String SPRING_TEST = "5.3.13";
+    public static final String LOMBOK = "1.18.22";
+}
